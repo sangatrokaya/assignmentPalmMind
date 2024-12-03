1274214e5fccfacf86257fc755b9b79628c54faf
Sangat Rokaya <rokayasangat123@gmail.com>
2024-12-03 21:06:45 +0545

    add: password encryption using bcryptjs

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,27 +1,29 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 const registerUser = async (req, res, next) => {
-  let { name, email, password, age } = req.body;
-  let userExists = await User.findOne({ email });
-  if (userExists) {
-    let err = new Error(`User with email ${email} already exists!`);
-    err.status = 400; // bad request
-    throw err;
-  } else {
-    let user = await User.create({
-      name,
-      email,
-      password,
-      age,
-    });
-    res.send({
-      message: "User registered successfully!",
-      user: {
-        name: user.name,
-        email: user.email,
-        age: user.age,
-      },
-    });
+  try {
+    let { email, password } = req.body;
+    let userExists = await User.findOne({ email });
+    if (userExists) {
+      let err = new Error(`User with email ${email} already exists!`);
+      err.status = 400; // bad request
+      throw err;
+    } else {
+      let salt = await bcrypt.genSalt(10);
+      let hashedPassword = await bcrypt.hash(password, salt);
+      let user = await User.create({ ...req.body, password: hashedPassword });
+      res.send({
+        message: "User registered successfully!",
+        user: {
+          name: user.name,
+          email: user.email,
+          age: user.age,
+        },
+      });
+    }
+  } catch (err) {
+    next(err);
   }
 };
 
